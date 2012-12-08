aebc503bb17d3fcda5edc4a37deccc4bb871d516
Arik Achmad <4k4tsuk1.27@gmail.com>
2012-12-08 22:54:19 +0700

    Add bg search

---
+++ PivotApp1/Result.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Mocoo
+{
+    public partial class Result : PhoneApplicationPage
+    {
+        public Result()
+        {
+            InitializeComponent();
+        }
+    }
+}
